b5cb0e18e62e080a5ffa9e79bb1b6fedca471913
SauloEspitia <sseg82@gmail.com>
2024-02-20 11:33:30 -0500

    cambio de ips

--- networking.tf
+++ networking.tf
@@ -4,21 +4,21 @@
 
 # Creación de VPC en AWS
 resource "aws_vpc" "vpcTerra" {
-  cidr_block = "20.0.0.0/24"
+  cidr_block = "30.0.0.0/24"
 }
 
 # Creación de Subred1 en AWS
 resource "aws_subnet" "subnetTerra1" {
   vpc_id                  = aws_vpc.vpcTerra.id
-  cidr_block              = "20.0.1.0/28"
+  cidr_block              = "30.0.0.0/28"
   availability_zone       = "us-east-1a"
 }
 
 # Creación de Subred2 en AWS
 resource "aws_subnet" "subnetTerra2" {
   vpc_id                  = aws_vpc.vpcTerra.id
-  cidr_block              = "20.0.2.0/28"
-  availability_zone       = "us-east-1a"
+  cidr_block              = "30.0.0.16/28"
+  availability_zone       = "us-east-1b"
 }
 
 # Creación Internet Gateway
@@ -29,7 +29,6 @@ resource "aws_internet_gateway" "igwTerra" {
 # Creación de Tabla de Rutas
 resource "aws_route_table" "rtTerra" {
   vpc_id = aws_vpc.vpcTerra.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igwTerra.id
--- proveedor.tf
+++ proveedor.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "5.37.0"
     }
   }
 }
@@ -10,9 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+#  access_key = "my-access-key"
+#  secret_key = "my-secret-key"
 }
-
-# Create a VPC
-#resource "aws_vpc" "example" {
-#  cidr_block = "10.0.0.0/16"
-#}
