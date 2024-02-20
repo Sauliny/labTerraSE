e610523e9986e36a2c8f2bac7741c575aa784ae6
SauloEspitia <sseg82@gmail.com>
2024-02-19 22:50:26 -0500

    ajuste networking

--- networking.tf
+++ networking.tf
@@ -7,16 +7,31 @@ resource "aws_vpc" "vpcTerra" {
   cidr_block = "20.0.0.0/24"
 }
 
-# Creacion de Subred1 en AWS
-resource "aws_subnet" "Subred1" {
-  vpc_id     = aws_vpc.Subred1.id
-  cidr_block = "20.0.1.0/28"
-  availability_zone = "us_east_1a"
+# Creación de Subred1 en AWS
+resource "aws_subnet" "subnetTerra1" {
+  vpc_id                  = aws_vpc.vpcTerra.id
+  cidr_block              = "20.0.1.0/28"
+  availability_zone       = "us-east-1a"
 }
 
-# Creacion de Subred2 en AWS
-resource "aws_subnet" "Subred2" {
-  vpc_id     = aws_vpc.Subred2.id
-  cidr_block = "20.0.2.0/28"
-  availability_zone = "us_east_1a"
+# Creación de Subred2 en AWS
+resource "aws_subnet" "subnetTerra2" {
+  vpc_id                  = aws_vpc.vpcTerra.id
+  cidr_block              = "20.0.2.0/28"
+  availability_zone       = "us-east-1a"
+}
+
+# Creación Internet Gateway
+resource "aws_internet_gateway" "igwTerra" {
+  vpc_id = aws_vpc.vpcTerra.id
+}
+
+# Creación de Tabla de Rutas
+resource "aws_route_table" "rtTerra" {
+  vpc_id = aws_vpc.vpcTerra.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igwTerra.id
+  }
 }
